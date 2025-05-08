ab267644d1ce8332ec58fba4ce494fd8ccc8528b
Chandini Mounika Chikkam <chikkamchandinimounika@gmail.com>
2025-05-08 15:06:15 +0530

    Update StaffViews.py

--- student_management_system/student_management_app/StaffViews.py
+++ student_management_system/student_management_app/StaffViews.py
@@ -2,7 +2,6 @@ import json
 from datetime import datetime
 from uuid import uuid4
 
-
 from django.contrib import messages
 from django.core import serializers
 from django.forms import model_to_dict
