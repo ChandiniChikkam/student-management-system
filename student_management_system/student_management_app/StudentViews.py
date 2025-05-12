50c961a565be8df51a8c6cb4f966947f7f9af4b2
Chandini Mounika Chikkam <chikkamchandinimounika@gmail.com>
2025-05-12 23:23:24 +0530

    Update StudentViews.py

--- student_management_system/student_management_app/StudentViews.py
+++ student_management_system/student_management_app/StudentViews.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
+
 from student_management_app.models import Students, Courses, Subjects, CustomUser, Attendance, AttendanceReport, \
     LeaveReportStudent, FeedBackStudent, NotificationStudent, StudentResult, OnlineClassRoom, SessionYearModel
 
