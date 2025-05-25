10eeb533adacad33b44b002f1ff8ae73afc4b6b7
Chandini Mounika Chikkam <chikkamchandinimounika@gmail.com>
2025-05-25 23:53:08 +0530

    Update EditResultVIewClass.py

--- student_management_system/student_management_app/EditResultVIewClass.py
+++ student_management_system/student_management_app/EditResultVIewClass.py
@@ -13,7 +13,6 @@ class EditResultViewClass(View):
         staff_id=request.user.id
         edit_result_form=EditResultForm(staff_id=staff_id)
         return render(request,"staff_template/edit_student_result.html",{"form":edit_result_form})
-
     
     def post(self,request,*args,**kwargs):
         form=EditResultForm(staff_id=request.user.id,data=request.POST)
